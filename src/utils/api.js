b51ade04f8978ddb7ba791d5f599a00869beac7e
Zhou <zhijun.zhou@hpe.com>
2018-03-19 15:26:27 +0800

    修改超时时间

--- src/utils/api.js
+++ src/utils/api.js
@@ -7,7 +7,7 @@ const baseURL = isProduction ? 'http://59.175.25.0:7777' : 'http://59.175.25.0:7
 const api = axios.create({
   baseURL: baseURL,
   // withCredentials: true,
-  timeout: 30000
+  timeout: 0
 })
 
 export default api
