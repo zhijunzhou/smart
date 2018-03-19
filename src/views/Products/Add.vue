780b4122e0dea1547040c5e6220ddf11a3e3eac8
Zhou <zhijun.zhou@hpe.com>
2018-03-19 20:03:58 +0800

    竞品编辑

--- src/views/Products/Add.vue
+++ src/views/Products/Add.vue
@@ -1,7 +1,7 @@
 <template v-if="product">
   <el-form :label-position="labelPosition" label-width="80px" class="product-form">
     <el-form-item label="产品S/N">
-      <el-input v-model="product.sn"></el-input>
+      <el-input v-model="product.asin"></el-input>
     </el-form-item>
     <el-form-item label="图片">
       <el-upload
@@ -15,11 +15,11 @@
       </el-upload>
     </el-form-item>
     <el-form-item label="描述">
-      <el-input type="textarea" v-model="product.description"></el-input>
+      <el-input type="textarea" v-model="product.name"></el-input>
     </el-form-item>
     <el-form-item label="关键字">
       <el-select
-        v-model="product.keywords"
+        v-model="product.classRanks"
         multiple
         filterable
         allow-create
@@ -27,33 +27,25 @@
         placeholder="请选择关键字"
         class="products-keywords-select">
         <el-option
-          v-for="item in product.keywords"
-          :key="item"
-          :label="item"
-          :value="item">
+          v-for="item in product.classRanks"
+          :key="item.class + '_' + item.rank"
+          :label="item.class"
+          :value="item.class">
         </el-option>
       </el-select>
     </el-form-item>
-    <el-form-item label="竞品" class="text-center">
-      <el-button size="mini" icon="el-icon-plus" round>增加竞品</el-button>
+    <el-form-item label="竞品">
+      <div v-for="cp in competitors" :key="cp.competitorId">
+        {{cp.competitorId}} <i class="el-icon-delete" @click="deleteCompetitor(cp.competitorId)"></i>
+      </div>
+      <el-button size="mini" icon="el-icon-plus" v-if="showAddButton" @click="addCompetitor" round>增加竞品</el-button>
+      <el-button size="mini" v-if="!showAddButton" @click="saveCompetitor" round>保存竞品</el-button>
     </el-form-item>
-    <el-form-item label="竞品S/N">
-      <el-input v-model="product.sn"></el-input>
-    </el-form-item>
-    <el-form-item label="竞品图片">
-      <el-upload
-        class="avatar-uploader"
-        action=""
-        :show-file-list="false"
-        :on-success="handleAvatarSuccess"
-        :before-upload="beforeAvatarUpload">
-        <img v-if="product.imgUrl" :src="product.imgUrl" class="avatar">
-        <i v-else class="el-icon-plus avatar-uploader-icon"></i>
-      </el-upload>
-    </el-form-item>
-    <el-form-item label="竞品描述">
-      <el-input type="textarea" v-model="product.description"></el-input>
-    </el-form-item>
+    <div v-if="!showAddButton">
+      <el-form-item label="竞品S/N">
+        <el-input v-model="competitor.competitorAsin"></el-input>
+      </el-form-item>
+    </div>
     <el-form-item class="text-center">
       <el-button type="primary" round>立即保存</el-button>
     </el-form-item>
@@ -61,16 +53,27 @@
 </template>
 
 <script>
+import api from '@/utils/api'
+
 export default {
   data () {
     return {
       product: {},
-      labelPosition: 'right'
+      competitors: [],
+      labelPosition: 'right',
+      showAddButton: true,
+      competitor: {
+        productId: '',
+        competitorAsin: '',
+        shopId: ''
+      }
     }
   },
   created () {
-    if (this.$route.query && this.$route.query.name) {
-      this.product = this.$route.query
+    let self = this
+    if (self.$route.query && self.$route.query.name) {
+      self.product = self.$route.query
+      self.listCompetitor()
     }
   },
   methods: {
@@ -88,6 +91,32 @@ export default {
         this.$message.error('上传头像图片大小不能超过 2MB!')
       }
       return isJPG && isLt2M
+    },
+    addCompetitor () {
+      let self = this
+      self.competitor.shopId = self.product.shopId
+      self.competitor.productId = self.product.asin
+      self.showAddButton = false
+    },
+    saveCompetitor () {
+      let self = this
+      api.post(`/api/product/competitor`, self.competitor).then(res => {
+        self.listCompetitor()
+      })
+    },
+    listCompetitor () {
+      let self = this
+      api.get(`/api/product/competitor/${self.product.shopId}/${self.product.asin}`).then(res => {
+        self.competitors = res.data
+      })
+    },
+    deleteCompetitor (id) {
+      if (confirm('Do you want to delete the competitor product?')) {
+        let self = this
+        api.delete(`/api/product/competitor/${self.product.shopId}/${self.product.asin}/${id}`).then(res => {
+          self.listCompetitor()
+        })
+      }
     }
   }
 }
