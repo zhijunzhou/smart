abbbf8f9ee3bca7b0456496356dfc8f0b108ec14
zhijunzhou <zhijun.zhou@hpe.com>
2018-03-31 23:37:04 +0800

    修改文字

--- src/views/workflow/workflow.vue
+++ src/views/workflow/workflow.vue
@@ -218,7 +218,7 @@
           'finished': '完成',
           'summed': '完结',
           'rejected': '拒绝',
-          'closed': '关闭'
+          'closed': '关闭工作'
         },
         chains: {
           issued: {
